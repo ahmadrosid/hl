ef19e451e50ca4684b804212820e71e2152e2dc7
ahmadrosid <alahmadrosid@gmail.com>
2021-12-18 00:52:32 +0700

    feat: setup cli app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("hl")
+        .version("1.0")
+        .author("Ahmad Rosid <alahmadrosid@gmail.com>")
+        .about("Syntax hightlighting.")
+        .arg(
+            arg!([filepath] "File path to parse.")
+        )
+        .subcommand(
+            App::new("test")
+                .about("does testing things")
+                .arg(arg!(-l --list "lists test values")),
+        )
+        .get_matches();
+
+    if let Some(filepath) = matches.value_of("filepath") {
+        println!("Value for filepath: {}", filepath);
+    }
+
+    if let Some(matches) = matches.subcommand_matches("test") {
+        // "$ myapp test" was run
+        if matches.is_present("list") {
+            // "$ myapp test -l" was run
+            println!("Printing testing lists...");
+        } else {
+            println!("Not printing testing lists...");
+        }
+    }
 }
