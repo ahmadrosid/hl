78f0530db7a901ca2ef5039a37941470bd4e623a
ahmadrosid <alahmadrosid@gmail.com>
2021-12-18 23:03:33 +0700

    feat: add initial html render

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use clap::{arg, App};
 use std::fs;
-// use std::path::Path;
 
 fn main() {
     let matches = App::new("hl")
         .version("1.0")
         .author("Ahmad Rosid <alahmadrosid@gmail.com>")
         .about("Syntax hightlighting.")
-        .arg(
-            arg!([filepath] "File path to parse.")
-        )
+        .arg(arg!([filepath] "File path to parse."))
         .subcommand(
             App::new("test")
                 .about("does testing things")
@@ -18,32 +15,63 @@ fn main() {
         .get_matches();
 
     if let Some(filepath) = matches.value_of("filepath") {
-        println!("Parsing filepath: {}\n", filepath);
-        parse_file_path(filepath)
-    }
-
-    if let Some(matches) = matches.subcommand_matches("test") {
-        // "$ myapp test" was run
-        if matches.is_present("list") {
-            // "$ myapp test -l" was run
-            println!("Printing testing lists...");
-        } else {
-            println!("Not printing testing lists...");
-        }
+        let s = parse_file_path(filepath);
+        println!("{}", s);
     }
 }
 
-fn parse_file_path(path: &str) {
-    let source_code =
-        fs::read(path).expect(&("Filed reading file ".to_owned() + path));
+fn parse_file_path(path: &str) -> String {
+    let source_code = fs::read(path).expect(&format!("Filed reading file {}", path));
     if Some(&source_code) == None {
-        return
+        return "".to_string();
     }
 
-    let s = match std::str::from_utf8(&source_code) {
-        Ok(v) => v,
-        Err(e) => panic!("Failed to parse source code to string {}", e),
-    };
+    let mut html = String::new();
+    // html.push_str("<!DOCTYPE html>\n");
+    // html.push_str("<html lang=\"en\">\n");
+    // html.push_str("<head>\n");
+    // html.push_str("  <meta charset=\"utf-8\">\n");
+    // html.push_str("  <link rel=\"stylesheet\" href=\"style.css\">\n");
+    // html.push_str("</head>\n");
+    // html.push_str("<body>\n");
+
+    html.push_str("<table class=\"highlight-table\">\n");
+    html.push_str("<tbody>\n");
     
-    println!("{}", s)
+    let mut i = 0;
+    let char_length = source_code.len();
+    let mut total = 1;
+    for n in source_code {
+        if i == 0 {
+            html.push_str("<tr data-line=\"1\">");
+            html.push_str("\n<td><div class=\"lineno\">1</div></td>\n");
+            html.push_str("<td><span>");
+            i = i + 1;
+        }
+
+        let c = n as char;
+        if c == '\n' {
+            i = i + 1;
+            if total != char_length {
+                html.push_str("</span>");
+                html.push_str("</td>\n</tr>");
+                html.push_str(&format!("\n<tr data-line=\"{}\">", i));
+                html.push_str(&format!("\n<td><div class=\"lineno\">{}</div></td>\n", i));
+                html.push_str("<td><span>\n");
+            } else {
+                html.push_str("</span>");
+                html.push_str("</td>\n</tr>");
+            }
+        }
+
+        html.push_str(&format!("{}", c));
+        total = total + 1;
+    }
+
+    html.push_str("</tbody>\n");
+    html.push_str("</table>\n");
+
+    // html.push_str("</body>\n");
+    // html.push_str("</html>\n");
+    html
 }
