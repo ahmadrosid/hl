d5b66c01abcb1ae8a6ddb8f5ddbf7a0a6d8a8841
ahmadrosid <alahmadrosid@gmail.com>
2021-12-25 14:42:55 +0700

    refactor: src/generator/token.rs

--- src/generator/token.rs
+++ src/generator/token.rs
@@ -1,6 +1,15 @@
 use yaml_rust::yaml::Hash;
-use crate::generator;
-use crate::generator::string::StringBuilder;
+use crate::generator::{
+    string::StringBuilder,
+    get_base,
+    get_constant,
+    get_entity,
+    get_entity_tag,
+    get_var,
+    get_skip,
+    get_keyword,
+    slash_comment_enable
+};
 
 pub fn generate_token(h: &Hash) -> String {
     let mut token = StringBuilder::new();
@@ -13,35 +22,35 @@ pub fn generate_token(h: &Hash) -> String {
     token.push_tabln(1, "CH(char),");
     token.push_tabln(1, "ENDL(char),");
 
-    for (k, _v) in generator::get_base(h) {
+    for (k, _v) in get_base(h) {
         token.push_tabln(1, &format!("{}(char),", k.as_str().unwrap()));
     }
 
-    for (k, _v) in generator::get_constant(h) {
+    for (k, _v) in get_constant(h) {
         token.push_tabln(1, &format!("{}(Vec<char>),", k.as_str().unwrap()));
     }
 
-    for (k, _v) in generator::get_var(h) {
+    for (k, _v) in get_var(h) {
         token.push_tabln(1, &format!("{}(Vec<char>),", k.as_str().unwrap()));
     }
 
-    for (k, _v) in generator::get_skip(h) {
+    for (k, _v) in get_skip(h) {
         token.push_tabln(1, &format!("{}(Vec<char>),", k.as_str().unwrap()));
     }
 
-    for (k, _v) in generator::get_entity(h) {
+    for (k, _v) in get_entity(h) {
         token.push_tabln(1, &format!("{}(Vec<char>),", k.as_str().unwrap()));
     }
 
-    for (k, _v) in generator::get_entity_tag(h) {
+    for (k, _v) in get_entity_tag(h) {
         token.push_tabln(1, &format!("{}(Vec<char>),", k.as_str().unwrap()));
     }
 
-    for (k, _v) in generator::get_keyword(h) {
+    for (k, _v) in get_keyword(h) {
         token.push_tabln(1, &format!("{}(Vec<char>),", k.as_str().unwrap()));
     }
 
-    if generator::slash_comment_enable(h) {
+    if slash_comment_enable(h) {
         token.push_tabln(1, "COMMENT(Vec<char>),");
     }
 
@@ -50,39 +59,39 @@ pub fn generate_token(h: &Hash) -> String {
     token.push_tabln(1, "let identifiers: String = identifier.into_iter().collect();");
     token.push_tabln(1, "match &identifiers[..] {");
 
-    for (k, v) in generator::get_constant(h) {
+    for (k, v) in get_constant(h) {
         if v.as_str().unwrap() != "" {
             token.push_str(&format!("\t\t\"{}\" => ", v.as_str().unwrap()));
             token.push_str(&format!("Ok(Token::{}(identifier.to_vec())),\n", k.as_str().unwrap()));
         }
     }
 
-    for (k, v) in generator::get_var(h) {
+    for (k, v) in get_var(h) {
         if v.as_str().unwrap() != "" {
             token.push_str(&format!("\t\t\"{}\" => ", v.as_str().unwrap()));
             token.push_str(&format!("Ok(Token::{}(identifier.to_vec())),\n", k.as_str().unwrap()));
         }
     }
 
-    for (k, v) in generator::get_entity(h) {
+    for (k, v) in get_entity(h) {
         if v.as_str().unwrap() != "" {
             token.push_str(&format!("\t\t\"{}\" => ", v.as_str().unwrap()));
             token.push_str(&format!("Ok(Token::{}(identifier.to_vec())),\n", k.as_str().unwrap()));
         }
     }
-    for (k, v) in generator::get_entity_tag(h) {
+    for (k, v) in get_entity_tag(h) {
         token.push_str(&format!("\t\t\"{}\" => ", v.as_str().unwrap()));
         token.push_str(&format!("Ok(Token::{}(identifier.to_vec())),\n", k.as_str().unwrap()));
     }
 
-    for (k, v) in generator::get_keyword(h) {
-        token.push_str(&format!("\t\t\"{}\" => ", v.as_str().unwrap()));
-        token.push_str(&format!("Ok(Token::{}(identifier.to_vec())),\n", k.as_str().unwrap()));
+    for (k, v) in get_keyword(h) {
+        token.push_tab(2,&format!("\"{}\" => ", v.as_str().unwrap()));
+        token.push_strln(&format!("Ok(Token::{}(identifier.to_vec())),", k.as_str().unwrap()));
     }
 
-    token.push_str("\t\t_ => Err(String::from(\"Not a keyword\"))\n");
-    token.push_str("\t}\n");
-    token.push_str("}\n");
+    token.push_tabln(2, "_ => Err(String::from(\"Not a keyword\"))");
+    token.push_tabln(1, "}");
+    token.push_strln("}");
 
     token.to_string()
 }
