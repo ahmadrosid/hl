b36cc51dec1e42702ed376f4eaeb6e15742ee185
ahmadrosid <alahmadrosid@gmail.com>
2021-12-25 14:18:35 +0700

    refactor: src/generator/render.rs

--- src/generator/render.rs
+++ src/generator/render.rs
@@ -5,40 +5,41 @@ use crate::generator::string::StringBuilder;
 pub fn generate_render_html(h: &Hash, name: String) -> String {
     let mut html = StringBuilder::new();
     html.push_strln("// ---- DON'T EDIT THIS IS AUTO GENERATED CODE ---- //");
-    html.push_str(&format!("use crate::lexers::{}::Lexer;\n", name));
-    html.push_str(&format!("use crate::lexers::{}::token;\n\n", name));
-    html.push_str("pub fn render_html(input: Vec<char>) -> String {\n");
-    html.push_str("\tlet mut l = Lexer::new(input);\n");
-    html.push_str("\tl.read_char();\n");
-    html.push_str("\tlet mut html = String::new();\n");
-    html.push_str("\tlet mut line = 1;\n");
-    html.push_str("\thtml.push_str(\"<table class=\\\"highlight-table\\\">\\n\");\n");
-    html.push_str("\thtml.push_str(\"<tbody>\\n\");\n");
-    html.push_str("\thtml.push_str(\"<tr>\");\n");
-    html.push_str("\thtml.push_str(&format!(\n");
-    html.push_str("\t\t\"<td class=\\\"hl-num\\\" data-line=\\\"{}\\\"></td><td>\",\n");
-    html.push_str("\t\tline\n\t));\n\n");
-    html.push_str("\tloop {\n");
-    html.push_str("\t\tlet token = l.next_token();\n");
-    html.push_str("\t\tif token == token::Token::EOF {\n");
-    html.push_str("\t\t\thtml.push_str(\"</td></tr>\\n\");\n");
-    html.push_str("\t\t\tbreak;\n\t\t}\n\n");
-    html.push_str("\t\tmatch token {\n");
+    html.push_strln(&format!("use crate::lexers::{}::Lexer;", name));
+    html.push_strln(&format!("use crate::lexers::{}::token;\n", name));
 
+    html.push_strln("pub fn render_html(input: Vec<char>) -> String {");
+    html.push_tabln(1, "let mut l = Lexer::new(input);");
+    html.push_tabln(1, "l.read_char();");
+    html.push_tabln(1, "let mut html = String::new();");
+    html.push_tabln(1, "let mut line = 1;");
+    html.push_tabln(1, "html.push_str(\"<table class=\\\"highlight-table\\\">\\n\");");
+    html.push_tabln(1, "html.push_str(\"<tbody>\\n\");");
+    html.push_tabln(1, "html.push_str(\"<tr>\");");
+    html.push_tabln(1, "html.push_str(&format!(");
+    html.push_tabln(2, "\"<td class=\\\"hl-num\\\" data-line=\\\"{}\\\"></td><td>\",");
+    html.push_tabln(2, "line\n\t));\n");
+
+    html.push_tabln(1, "loop {");
+    html.push_tabln(2, "let token = l.next_token();");
+    html.push_tabln(2, "if token == token::Token::EOF {");
+    html.push_tabln(3, "html.push_str(\"</td></tr>\\n\");");
+    html.push_tabln(3, "break;");
+    html.push_tabln(2, "}\n");
+
+    html.push_tabln(2, "match token {");
     for (k, _v) in generator::get_base(h) {
         let key = k.as_str().unwrap();
         if key != "ENDL" {
-            html.push_str("\t\t\t");
-            html.push_str(&format!("token::Token::{}(value)", key));
-            html.push_str(" => {\n");
-            html.push_str("\t\t\t\thtml.push(value);\n");
-            html.push_str("\t\t\t}\n");
+            html.push_tabln(3, &format!("token::Token::{}(value) => {{", key));
+            html.push_tabln(4, "html.push(value);");
+            html.push_tabln(3, "}");
         }
     }
 
-    html.push_str("\t\t\ttoken::Token::CH(value) => {\n");
-    html.push_str("\t\t\t\thtml.push(value);\n");
-    html.push_str("\t\t\t}\n");
+    html.push_tabln(3, "token::Token::CH(value) => {");
+    html.push_tabln(4, "html.push(value);");
+    html.push_tabln(3, "}");
 
     if generator::slash_comment_enable(h) {
         html.push_tabln(3, "token::Token::COMMENT(value) => {");
@@ -62,56 +63,44 @@ pub fn generate_render_html(h: &Hash, name: String) -> String {
     }
 
     for (k, _v) in generator::get_skip(h) {
-        html.push_str("\t\t\t");
-        html.push_str(&format!("token::Token::{}(value)", k.as_str().unwrap()));
-        html.push_str(" => {\n");
-        html.push_str("\t\t\t\thtml.push_str(&value.iter().collect::<String>());\n");
-        html.push_str("\t\t\t}\n");
+        html.push_tabln(3, &format!("token::Token::{}(value) => {{", k.as_str().unwrap()));
+        html.push_tabln(4, "html.push_str(&value.iter().collect::<String>());");
+        html.push_tabln(3, "}");
     }
 
     for (k, _v) in generator::get_entity(h) {
-        html.push_str("\t\t\t");
-        html.push_str(&format!("token::Token::{}(value)", k.as_str().unwrap()));
-        html.push_str(" => {\n");
-        html.push_str("\t\t\t\thtml.push_str(&format!(\"<span class=\\\"hl-en\\\">{}</span>\", \
-        value.iter().collect::<String>()));\n");
-        html.push_str("\t\t\t}\n");
+        html.push_tabln(3,&format!("token::Token::{}(value) => {{", k.as_str().unwrap()));
+        html.push_tabln(4, "html.push_str(&format!(\"<span class=\\\"hl-en\\\">{}</span>\", \
+        value.iter().collect::<String>()));");
+        html.push_tabln(3, "}");
     }
 
     for (k, _v) in generator::get_entity_tag(h) {
-        html.push_str("\t\t\t");
-        html.push_str(&format!("token::Token::{}(value)", k.as_str().unwrap()));
-        html.push_str(" => {\n");
-        html.push_str("\t\t\t\thtml.push_str(&format!(\"<span class=\\\"hl-ent\\\">{}</span>\", \
-        value.iter().collect::<String>()));\n");
-        html.push_str("\t\t\t}\n");
+        html.push_tabln(3,&format!("token::Token::{}(value) => {{", k.as_str().unwrap()));
+        html.push_tabln(4, "html.push_str(&format!(\"<span class=\\\"hl-ent\\\">{}</span>\", \
+        value.iter().collect::<String>()));");
+        html.push_tabln(3, "}");
     }
 
     for (k, _v) in generator::get_constant(h) {
-        html.push_str("\t\t\t");
-        html.push_str(&format!("token::Token::{}(value)", k.as_str().unwrap()));
-        html.push_str(" => {\n");
-        html.push_str("\t\t\t\thtml.push_str(&format!(\"<span class=\\\"hl-c\\\">{}</span>\", \
-        value.iter().collect::<String>()));\n");
-        html.push_str("\t\t\t}\n");
+        html.push_tabln(3,&format!("token::Token::{}(value) => {{", k.as_str().unwrap()));
+        html.push_tabln(4, "html.push_str(&format!(\"<span class=\\\"hl-c\\\">{}</span>\", \
+        value.iter().collect::<String>()));");
+        html.push_tabln(3, "}");
     }
 
     for (k, _v) in generator::get_var(h) {
-        html.push_str("\t\t\t");
-        html.push_str(&format!("token::Token::{}(value)", k.as_str().unwrap()));
-        html.push_str(" => {\n");
-        html.push_str("\t\t\t\thtml.push_str(&format!(\"<span class=\\\"hl-v\\\">{}</span>\", \
-        value.iter().collect::<String>()));\n");
-        html.push_str("\t\t\t}\n");
+        html.push_tabln(3,&format!("token::Token::{}(value) => {{", k.as_str().unwrap()));
+        html.push_tabln(4, "html.push_str(&format!(\"<span class=\\\"hl-v\\\">{}</span>\", \
+        value.iter().collect::<String>()));");
+        html.push_tabln(3, "}");
     }
 
     for (k, _v) in generator::get_keyword(h) {
-        html.push_str("\t\t\t");
-        html.push_str(&format!("token::Token::{}(value)", k.as_str().unwrap()));
-        html.push_str(" => {\n");
-        html.push_str("\t\t\t\thtml.push_str(&format!(\"<span class=\\\"hl-k\\\">{}</span>\", \
-        value.iter().collect::<String>()));\n");
-        html.push_str("\t\t\t}\n");
+        html.push_tabln(3,&format!("token::Token::{}(value) => {{", k.as_str().unwrap()));
+        html.push_tabln(4, "html.push_str(&format!(\"<span class=\\\"hl-k\\\">{}</span>\", \
+        value.iter().collect::<String>()));");
+        html.push_tabln(3, "}");
     }
 
     html.push_str("\t\t\ttoken::Token::ENDL(_) => {\n\t\t\t\t\
@@ -127,11 +116,12 @@ pub fn generate_render_html(h: &Hash, name: String) -> String {
     l.read_char();\n\t\t\t\
     }\n");
 
-    html.push_str("\t\t}\n");
-    html.push_str("\t}\n\n");
-    html.push_str("\thtml.push_str(\"</tbody>\\n\");\n");
-    html.push_str("\thtml.push_str(\"</table>\");\n");
-    html.push_str("\thtml\n");
-    html.push_str("}\n");
+    html.push_tabln(2, "}");
+    html.push_tabln(1, "}\n");
+
+    html.push_tabln(1, "html.push_str(\"</tbody>\\n\");");
+    html.push_tabln(1, "html.push_str(\"</table>\");");
+    html.push_tabln(1, "html");
+    html.push_strln("}");
     html.to_string()
 }
