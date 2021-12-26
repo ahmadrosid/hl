5b4ae5288cddf93aa0ce073ac657432b33701a67
ahmadrosid <alahmadrosid@gmail.com>
2021-12-26 12:21:37 +0700

    test: unit test for css and golang

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,6 +1,5 @@
-
+use crate::lexers::{css, go, rust};
 use std::fs;
-use crate::lexers::rust;
 
 fn read_input(path: &str) -> Vec<char> {
     let source = fs::read(path).expect(&format!("Filed reading file {}", path));
@@ -10,6 +9,22 @@ fn read_input(path: &str) -> Vec<char> {
 fn read_file(path: &str) -> String {
     let source = fs::read_to_string(path).expect(&format!("Filed reading file {}", path));
     return source;
+}
+
+#[test]
+fn test_css() {
+    let input = read_input("src/test/testdata/input/css.css");
+    let expected = read_file("src/test/testdata/output/css.html");
+    let actual = css::render::render_html(input);
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_golang() {
+    let input = read_input("src/test/testdata/input/golang.go");
+    let expected = read_file("src/test/testdata/output/golang.html");
+    let actual = go::render::render_html(input);
+    assert_eq!(expected, actual);
 }
 
 #[test]
