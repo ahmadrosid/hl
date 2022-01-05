306a0e8990f8271c65e814caff8b77cd4a3f5c60
ahmadrosid <alahmadrosid@gmail.com>
2022-01-05 21:18:58 +0700

    feat: update bash lexer constant

--- src/lexers/bash/token.rs
+++ src/lexers/bash/token.rs
@@ -17,14 +17,17 @@ pub enum Token {
 pub fn get_keyword_token(identifier: &Vec<char>) -> Result<Token, String> {
     let identifiers: String = identifier.into_iter().collect();
     match &identifiers[..] {
+        "compgen" => Ok(Token::CONSTANT(identifier.to_vec())),
         "echo" => Ok(Token::CONSTANT(identifier.to_vec())),
         "eval" => Ok(Token::CONSTANT(identifier.to_vec())),
         "exit" => Ok(Token::CONSTANT(identifier.to_vec())),
+        "false" => Ok(Token::CONSTANT(identifier.to_vec())),
         "kill" => Ok(Token::CONSTANT(identifier.to_vec())),
         "read" => Ok(Token::CONSTANT(identifier.to_vec())),
         "source" => Ok(Token::CONSTANT(identifier.to_vec())),
         "unset" => Ok(Token::CONSTANT(identifier.to_vec())),
         "test" => Ok(Token::CONSTANT(identifier.to_vec())),
+        "true" => Ok(Token::CONSTANT(identifier.to_vec())),
         "case" => Ok(Token::KEYWORD(identifier.to_vec())),
         "continue" => Ok(Token::KEYWORD(identifier.to_vec())),
         "do" => Ok(Token::KEYWORD(identifier.to_vec())),
