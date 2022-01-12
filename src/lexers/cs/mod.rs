9cd9e6e2b2cd1a7b97235bfcd73dc373f0a463b0
ahmadrosid <alahmadrosid@gmail.com>
2022-01-12 12:04:45 +0700

    feat(cs): add ? as singgle keyword

--- src/lexers/cs/mod.rs
+++ src/lexers/cs/mod.rs
@@ -160,6 +160,9 @@ impl Lexer {
             '=' => {
                 tok = token::Token::KEYWORD(vec![self.ch]);
             }
+            '?' => {
+                tok = token::Token::KEYWORD(vec![self.ch]);
+            }
             '/' => {
                 if self.input[self.position + 1] == '/' {
                     tok = token::Token::COMMENT(read_slash_comment(self));
