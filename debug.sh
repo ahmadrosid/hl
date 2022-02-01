acc0cba666635b3cc6e57de9bddf3fa28b9547b2
ahmadrosid <alahmadrosid@gmail.com>
2022-02-02 04:38:30 +0700

    fix: wrong rules for zig

--- debug.sh
+++ debug.sh
@@ -5,3 +5,7 @@ cargo run generate "rules/zig.yml"
 #cargo run src/test/testdata/input/bash.sh.stub -l haskell > table.html
 cargo run test-file -l zig > table.html
 cargo fmt
+
+# Copy result
+# cp table.html src/test/testdata/output/zig.html.stub
+# cp test-file src/test/testdata/input/zig.zig.stub
