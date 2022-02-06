04d6a6cf6cb927c1efe1989023b59b0531af7d77
ahmadrosid <alahmadrosid@gmail.com>
2022-02-06 21:23:42 +0700

    feat(nim): add more keyword

--- debug.sh
+++ debug.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #source ./build.sh
-cargo run generate "rules/bash.yml"
+cargo run generate "rules/java.yml"
 #cargo run src/test/testdata/input/bash.sh.stub -l bash > table.html
 #cargo run src/test/testdata/input/bash.sh.stub -l haskell > table.html
-cargo run test-file -l bash > table.html
+cargo run test-file -l java > table.html
 cargo fmt
 
 # Copy result
-# cp table.html src/test/testdata/output/kotlin.html.stub
-# cp test-file src/test/testdata/input/kotlin.kt.stub
+cp table.html src/test/testdata/output/java.html.stub
+cp test-file src/test/testdata/input/java.java.stub
