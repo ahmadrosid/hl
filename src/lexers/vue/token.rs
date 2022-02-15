090b6b2f7318441346bd58d5662d1820d6429d7b
ahmadrosid <alahmadrosid@gmail.com>
2022-02-15 16:11:25 +0700

    feat: add vue lexer

---
+++ src/lexers/vue/token.rs
@@ -0,0 +1,11 @@
+// ---- DON'T EDIT! THIS IS AUTO GENERATED CODE ---- //
+use crate::lexers::Token;
+
+pub fn get_keyword_token(identifier: &Vec<char>) -> Result<Token, String> {
+    let id: String = identifier.into_iter().collect();
+    match &id[..] {
+        "template" => Ok(Token::ENTITYTAG(identifier.to_vec())),
+        "script" => Ok(Token::ENTITYTAG(identifier.to_vec())),
+        _ => Err(String::from("Not a keyword")),
+    }
+}
