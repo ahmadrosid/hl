5d11f3b8350fde72d523b6a63a9874740230d506
ahmadrosid <alahmadrosid@gmail.com>
2022-03-20 19:24:33 +0700

    feat: add example files

---
+++ examples/html.rs
@@ -0,0 +1,18 @@
+use std::path::Path;
+
+use hl::render_html;
+use std::fs::read_to_string;
+
+// cargo run --package hl --example html
+fn main() {
+    let file_path = std::env::args().nth(1).unwrap_or(String::new());
+    let path = Path::new(&file_path);
+    if !path.exists() {
+        println!("Require correct file path!");
+        std::process::exit(1);
+    }
+
+    let input = read_to_string(path).unwrap().chars().collect::<Vec<_>>();
+    let result = render_html(input, "raw");
+    println!("{}", result)
+}
