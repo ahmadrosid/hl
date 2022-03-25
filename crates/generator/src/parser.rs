72210c5e69d67887dfa6724c1070a7257639c26a
ahmadrosid <alahmadrosid@gmail.com>
2022-03-25 14:17:49 +0700

    core: tidy up

--- crates/generator/src/parser.rs
+++ crates/generator/src/parser.rs
@@ -85,6 +85,7 @@ get_hash!(get_condition, condition);
 
 get_string!(get_multi_line_comment, multi_line_comment);
 get_string!(get_multi_line_string, multi_line_string);
+
 get_bool!(bracket_dash_comment_enable, bracket_dash_comment);
 
 pub trait ConditionExt {
