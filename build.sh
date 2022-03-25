ac0ea2a1640c387eaf8427750ada65984e6225bb
ahmadrosid <alahmadrosid@gmail.com>
2022-03-25 13:12:29 +0700

    feat: update build script

--- build.sh
+++ build.sh
@@ -2,46 +2,7 @@
 # set -e
 cargo build -q --workspace -p generator
 
-array=(
-  "rules/actionscript.yml"
-  "rules/bash.yml"
-  "rules/c.yml"
-  "rules/clojure.yml"
-  "rules/css.yml"
-  "rules/cuda.yml"
-  "rules/dart.yml"
-  "rules/edn.yml"
-  "rules/erlang.yml"
-  "rules/cpp.yml"
-  "rules/cs.yml"
-  "rules/go.yml"
-  "rules/groovy.yml"
-  "rules/haskell.yml"
-  "rules/html.yml"
-  "rules/java.yml"
-  "rules/javascript.yml"
-  "rules/json.yml"
-  "rules/kotlin.yml"
-  "rules/lua.yml"
-  "rules/makefile.yml"
-  "rules/markdown.yml"
-  "rules/nim.yml"
-  "rules/php.yml"
-  "rules/python.yml"
-  "rules/raw.yml"
-  "rules/ruby.yml"
-  "rules/rust.yml"
-  "rules/toml.yml"
-  "rules/typescript.yml"
-  "rules/vue.yml"
-  "rules/yaml.yml"
-  "rules/zig.yml"
-)
-
-for i in "${array[@]}"
-do
-	./target/debug/hl-gen -i $i -o crates/core;
-done
+find rules/ -type f -name "*.yml" | xargs -I % sh -c './target/debug/hl-gen -i % -o crates/core;'
 
 cargo fmt
 cargo test --workspace -p hl_tests
