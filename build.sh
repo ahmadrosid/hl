6583114053b892a96f195bba9f6a084c89a037a6
ahmadrosid <alahmadrosid@gmail.com>
2022-03-25 13:14:09 +0700

    core: tidy up

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # set -e
 cargo build -q --workspace -p generator
-
 find rules/ -type f -name "*.yml" | xargs -I % sh -c './target/debug/hl-gen -i % -o crates/core;'
-
 cargo fmt
 cargo test --workspace -p hl_tests
 git status
