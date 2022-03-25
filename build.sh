1bd45de45e78da148f22e7ac463ae733ba94c4b1
ahmadrosid <alahmadrosid@gmail.com>
2022-03-25 13:57:38 +0700

    core: tidy up

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cargo build -q --workspace -p generator
 find rules/ -type f -name "*.yml" | xargs -I % sh -c './target/debug/hl-gen -i % -o crates/core;'
 cargo fmt
 cargo test --workspace -p hl_tests
+find rules/ -type f -name "*.yml" | wc -l | xargs -I % sh -c 'echo "Total lexers generated: %";'
 git status
